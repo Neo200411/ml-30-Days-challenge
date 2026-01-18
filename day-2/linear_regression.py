ca77215943053380268ddba1bdf3bf20044ef5f3
Neo200411 <kneom1234@gmail.com>
2026-01-19 00:07:42 +0530

    Implement linear regression model with predictions

--- day-2/linear_regression.py
+++ day-2/linear_regression.py
@@ -1,1 +1,15 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Slope:", model.coef_[0])
+print("Intercept:", model.intercept_)
+
+prediction = model.predict([[6]])
+print("Prediction for x = 6:", prediction[0])
 
